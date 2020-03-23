58258f86d9754d40e5400ba63fb915419f0bc7b5
Paul <you@example.com>
2020-03-23 21:04:07 +0700

    init

---
+++ blocks/aside.php
@@ -0,0 +1,13 @@
+<aside class="col-md-4">
+  <div class="p-3 mb-3 bg-warning rounded">
+    <h4><b>Интересные факты</b></h4>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      Quidem optio omnis soluta repudiandae non quaerat vero l
+      aboriosam, temporibus dolores, ea deserunt asperiores ipsa
+      obcaecati quisquam ipsum recusandae molestiae reiciendis
+      earum.</p>
+  </div>
+  <div class="p-3 mb-3">
+    <img class="img-thumbnail" src="/img/Kill-la-Kill.jpg">
+  </div>
+</aside>
---
+++ error.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Ошибка 404</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <link rel="stylesheet" href="/css/main.css">
+  <link rel="icon" href="/img/icon.ico">
+</head>
+<body>
+  <?php require 'blocks/header.php'; ?>
+  Ошибка 404, перейдите <a href="/">на главную</a>
+  <?php require 'blocks/footer.php'; ?>
+</body>
+</html>
